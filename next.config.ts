64fa5dceb19273f5720a738461fbecb1a51c59d4
Lai Yi jhe <laiyijhe@google.com>
2026-03-20 00:17:25 +0800

    clean up config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // 強制關閉可能會出錯的加速引擎，改用穩定模式
-  transpilePackages: ["@next/swc-win32-x64-msvc"],
-  experimental: {
-    turbo: false as any
-  }
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
